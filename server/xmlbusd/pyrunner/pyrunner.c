89036d44712dd46e12da8affda4d9ce7e0c15034
Olger Warnier <o.warnier@thenewmotion.com>
2009-11-17 08:44:42 +0000

    Changes in threading in order to prevent crashes under heavy load

--- server/xmlbusd/pyrunner/pyrunner.c
+++ server/xmlbusd/pyrunner/pyrunner.c
@@ -3,7 +3,7 @@
  *  xmlbus
  *
  *  Created by Olger Warnier on 1/7/07.
- *  Copyright 2007 __MyCompanyName__. All rights reserved.
+ *  Copyright 2007 xmlbus.org. All rights reserved.
  *
  */
 
@@ -22,7 +22,9 @@ struct xmlbus_servicecustomdata_struct {
     xmlChar* pyModuleName;
     // add module as python object here
 };
-	
+
+PyThreadState *mainThreadState;
+
 /* internal helper functions to convert from and to python */
 xmlbusErrorPtr createPyXmlbusSoapMessage(xmlbusSoapMessagePtr soapMsg, PyObject** pySoapMessage)
 {
@@ -71,11 +73,12 @@ xmlbusErrorPtr xmlbusdRunnerInitialize(xmlNodePtr config,xmlbusdServiceConfig** 
     
     Py_Initialize();
     PyEval_InitThreads();
+    mainThreadState = PyThreadState_Get();
     PyRun_SimpleString("import sys \n");
     PyRun_SimpleString("sys.path.append('.\')\n");
     PyRun_SimpleString("print sys.path \n");
-         
-    return xbErr;
+	PyEval_ReleaseLock();
+	
 onError:   
     return xbErr;
 }
@@ -93,58 +96,6 @@ xmlbusErrorPtr serviceInitializeFunc(xmlbusdServiceConfig* serviceConf, xmlNodeP
     // the implemenation states the service to run. 
     newCustomData->pyModuleName = xmlStrdup(serviceConf->implementation);
     xmlbusServiceSetCustomData(&((serviceConf)->service),&newCustomData);
-/*
-    pModule = PyImport_ImportModule((char*)serviceCustomData->pyModuleName);
-    //Py_DECREF(pName);    
-    if (pModule != NULL) {
-        pDict = PyModule_GetDict(pModule);
-        // pDict is a borrowed reference
-        pFunc = PyDict_GetItemString(pDict, (char*)actionToExecute);
-        // pFun: Borrowed reference 
-        if (pFunc && PyCallable_Check(pFunc)) {
-            pArgs = PyTuple_New(1);
-            PyTuple_SetItem(pArgs, 0, requestAsPyObj);            
-            pValue = PyObject_CallObject(pFunc, pArgs);
-            //Py_DECREF(pArgs);
-            if (PyErr_Occurred()) {
-                PyErr_Print();
-                // xbErr = xmlbusErrorAdd()
-                Py_DECREF(pArgs);
-                Py_DECREF(pModule);
-                goto onError;
-            }
-
-            if (pValue != NULL) {
-                // convert back to soapmsgptr
-                //printf("Result of call: %ld\n", PyInt_AsLong(pValue));
-                newResponse = getXmlbusSoapMessagePtr(pValue);
-                *response = newResponse;
-                // TODO (done, check if it works) need to find a way to release the python object, but keep the C soapmessage struct
-                unlinkPyObjectFromSoapMessagePtr(pValue);
-                Py_DECREF(pValue);
-            } else {
-                //  xbErr = xmlbusErrorAdd()
-                Py_DECREF(pModule);
-                goto onError;
-            }
-            // pDict and pFunc are borrowed and must not be Py_DECREF-ed
-        } else {
-            PyObject* foundError = NULL;
-            foundError = PyErr_Occurred();
-            if (foundError != NULL) {
-                PyErr_Print();
-            }
-            xbErr = xmlbusErrorAdd(xbErr, XMLBUS_ERRORS_LOCATION, -1, BAD_CAST "Cannot find function '%s.%s()'", serviceCustomData->pyModuleName, actionToExecute); 
-            Py_DECREF(pModule);
-            goto onError;
-        }
-        Py_DECREF(pModule); // remove ref if not passed via goto onError
-    } else {
-        xbErr = xmlbusErrorAdd(xbErr, XMLBUS_ERRORS_LOCATION, -1, BAD_CAST "Could not import python module '%s'", serviceCustomData->pyModuleName);
-    }
-
-*/    
-    return xbErr;
 onError:
     return xbErr;
 }
@@ -153,6 +104,8 @@ onError:
 xmlbusErrorPtr xmlbusdRunnerFinalize() {
     //xmlbusServiceCustomDataPtr customData = xmlbusServiceGetCustomData(<#xmlbusServicePtr service#>);
     //xmlFree(customData->pyModuleName);
+	PyEval_AcquireLock();
+	PyThreadState_Swap(mainThreadState);
     Py_Finalize();
 }
 
@@ -172,10 +125,10 @@ xmlbusErrorPtr serviceExecutionFunc(xmlbusServicePtr service, xmlbusSoapMessageP
     PyObject *pFunc = NULL;
     PyObject *pArgs = NULL;
     PyObject *pValue = NULL;
-    PyThreadState *mainThreadState = NULL;
+	PyInterpreterState *mainInterpreterState;
+ 
     PyThreadState *myThreadState = NULL;
     PyThreadState *tempState = NULL;
-    PyInterpreterState *mainInterpreterState = NULL;
     int result = -1;
 	
 	serviceCustomData = xmlbusServiceGetCustomData(service);
@@ -191,26 +144,19 @@ xmlbusErrorPtr serviceExecutionFunc(xmlbusServicePtr service, xmlbusSoapMessageP
 	}
 	actionToExecute = xmlStrdup(foundActionNode->name);
 	actionToExecute = xmlStrncat(actionToExecute, BAD_CAST "Action",6);
-    
-    mainThreadState = PyThreadState_Get();
-    
-    // Get a reference to the PyInterpreterState
-    mainInterpreterState = mainThreadState->interp;
-    
-    // Create a thread state object for this thread
-    myThreadState = PyThreadState_New(mainInterpreterState);
-    
-    // Release global lock
-    PyEval_ReleaseLock();
-    
-    // Acquire global lock
-    PyEval_AcquireLock();
-    
-    // Swap in my thread state
-    tempState = PyThreadState_Swap(myThreadState);
-    
-    //PyRun_SimpleString("print sys.path \n");
-    
+
+	// create a threadstate object for this thread. 
+	PyEval_AcquireLock();
+	mainInterpreterState = mainThreadState->interp;
+	PyThreadState_Swap(mainThreadState);
+	myThreadState = PyThreadState_New(mainInterpreterState);
+	PyEval_ReleaseLock();
+
+	// now lock and run the actual service call. 
+	PyEval_AcquireLock();
+	PyThreadState_Swap(myThreadState);
+	
+	
     xbErr = createPyXmlbusSoapMessage(request, &requestAsPyObj);
     if (xbErr != XMLBUS_OK) {
         // error
@@ -219,8 +165,11 @@ xmlbusErrorPtr serviceExecutionFunc(xmlbusServicePtr service, xmlbusSoapMessageP
     
     //pName = PyString_FromString((char*)moduleName);
     /* Error checking of pName left out */
-    pModule = PyImport_ImportModule((char*)serviceCustomData->pyModuleName);
+	char* moduleName = strdup((char*)serviceCustomData->pyModuleName);
+    pModule = PyImport_ImportModuleNoBlock(moduleName);
+	free(moduleName);
     //Py_DECREF(pName);    
+
     if (pModule != NULL) {
         pDict = PyModule_GetDict(pModule);
         /* pDict is a borrowed reference */
@@ -267,28 +216,15 @@ xmlbusErrorPtr serviceExecutionFunc(xmlbusServicePtr service, xmlbusSoapMessageP
     } else {
         xbErr = xmlbusErrorAdd(xbErr, XMLBUS_ERRORS_LOCATION, -1, BAD_CAST "Could not import python module '%s'", serviceCustomData->pyModuleName);
     }
-    //Py_DECREF(pName);
-    PyThreadState_Swap(tempState);
-    
-    // Release global lock
-    PyEval_ReleaseLock();
-    
-    // Clean up thread state
-    PyThreadState_Clear(myThreadState);
-    PyThreadState_Delete(myThreadState);
-    
 	xmlFree(actionToExecute);
-	return xbErr;
 onError:
-    if (tempState != NULL) {
-        PyThreadState_Swap(tempState);
-    
-        // Release global lock
-        PyEval_ReleaseLock();
-    
-        // Clean up thread state
-        PyThreadState_Clear(myThreadState);
-        PyThreadState_Delete(myThreadState);
-    }
+	PyThreadState_Swap(NULL);
+	PyEval_ReleaseLock();
+	// clean up
+	PyEval_AcquireLock();
+	PyThreadState_Swap(NULL);
+	PyThreadState_Clear(myThreadState);
+	PyThreadState_Delete(myThreadState);
+	PyEval_ReleaseLock();
     return xbErr;
 }
